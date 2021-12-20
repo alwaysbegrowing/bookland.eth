53fa69b2339847665946fb042d59883afc64d84e
Russell <russell@deephire.com>
2021-12-20 14:05:32 -0600

    add web3 modal

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Page, User, Loading } from '@geist-ui/react';
+import { Page, User, Loading, Button, Grid, Spacer, Image } from '@geist-ui/react';
 import useSWR from 'swr';
+import useWeb3Modal from './hooks/useWeb3Modal';
+import WalletButton from './components/WalletButton';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function App() {
+  const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+
   const { data, error } = useSWR(
     'https://metadata.ens.domains/mainnet/avatar/bookland.eth/meta',
     fetcher
@@ -17,11 +21,25 @@ function App() {
   }
 
   return (
-    <Page>
+    <Page dotBackdrop>
+      <Page.Header>
+        <Grid.Container justify='flex-end'>
+          <Grid>
+            <Spacer h={1} />
+            <WalletButton
+              provider={provider}
+              loadWeb3Modal={loadWeb3Modal}
+              logoutOfWeb3Modal={logoutOfWeb3Modal}
+            />
+          </Grid>
+        </Grid.Container>
+      </Page.Header>
       <Page.Content>
         <User src={data.image} name='Bookland'>
           <User.Link href='https://etherscan.io/address/bookland.eth'>@bookland.eth</User.Link>
         </User>
+        <Button type='secondary' auto>Donate</Button>
+      
       </Page.Content>
     </Page>
   );
